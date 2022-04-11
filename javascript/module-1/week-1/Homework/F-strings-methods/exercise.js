46ff3c196dd86c802394d8b721a1a6f0a0268f9d
Ali Ashraf <adaydreamer10@gmail.com>
2022-04-11 03:02:03 +0200

    string methods

--- javascript/module-1/week-1/Homework/F-strings-methods/exercise.js
+++ javascript/module-1/week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,4 @@
 // Start by creating a variable `message`
-
+const name = "Daniel"
+const message = `My name is ${name} and my name is ${name.length} characters long`
 console.log(message);
--- javascript/module-1/week-1/Homework/F-strings-methods/exercise2.js
+++ javascript/module-1/week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,4 @@
 const name = " Daniel  ";
-
+const trimedName = name.trim()
+const message = `My name is ${trimedName} and my name is ${trimedName.length} characters long`
 console.log(message);
